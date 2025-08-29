fd5f584d1c392e8cdfccdabebb381ae2fefc6e3e
Britton Horn <bhorn@trinity.edu>
2025-08-29 12:38:02 -0500

    Added code from 8/29 lecture.

--- Section1FirstProject/Assets/Scripts/PlayerController.cs
+++ Section1FirstProject/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
+
 using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector2 movement;
+    [HideInInspector]
+    public float movement;
+    private bool isJumping;
+    public float jumpHeight;
+    public LayerMask groundLayer;
     Rigidbody2D rb;
+    [SerializeField]
+    private float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,12 +21,43 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = movement * 5f;
+        rb.linearVelocity = new Vector2(movement * speed, rb.linearVelocity.y);
+        //Debug.Log("Current Velocity: " + rb.linearVelocity.magnitude);
+        if (isJumping)
+        {
+            rb.AddForce(Vector2.up * jumpHeight);
+            isJumping = false;
+        }
     }
 
     void OnMove(InputValue value)
     {
-        Debug.Log("Move action triggered: " + value.Get<Vector2>());
-        movement = value.Get<Vector2>();
+        //Debug.Log("Move action triggered: " + value.Get<Vector2>());
+        movement = value.Get<float>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!isJumping && value.isPressed && isGrounded())
+        {
+            isJumping = true;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, groundLayer);
+        if (hit.collider != null)
+        {
+            //Debug.Log("Grounded on: " + hit.collider.name);
+            isJumping = false;
+            return true;
+        }
+        return false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered by: " + collision.name);
     }
 }
--- Section2FirstProject/Assets/Scripts/PlayerController.cs
+++ Section2FirstProject/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,17 @@
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector2 movement;
+    [HideInInspector]
+    public float movement;
+    private bool isJumping;
+    public LayerMask groundLayer;
     Rigidbody2D rb;
+    [SerializeField]
+    private float speed;
+    public float jumpForce;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,12 +20,45 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = movement * 5f;
+        rb.linearVelocity = new Vector2(movement * speed, rb.linearVelocity.y);
+        if (isJumping)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            isJumping = false;
+        }
+        // tons of mods
+        //rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, speed);
+        //Debug.Log("velocity: " + rb.linearVelocity.magnitude);
     }
 
     void OnMove(InputValue value)
     {
-        Debug.Log("move: " + value.Get<Vector2>());
-        movement = value.Get<Vector2>();
+        //Debug.Log("move: " + value.Get<Vector2>());
+        movement = value.Get<float>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!isJumping && value.isPressed && isGrounded())
+        {
+            isJumping = true;
+        }
+
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, groundLayer);
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.name);
+            return true;
+        }
+        return false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("triggered by " + collision.name);
     }
 }
